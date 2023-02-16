0f11d94933ad18b93917798d3f6752006955cba5
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2023-02-15 18:49:28 -0800

    Add files via upload

--- tourController.js
+++ tourController.js
@@ -36,7 +36,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id).populate('reviews');
 
     if(!tour) {
-        return next(new AppError('No tour found with that ID', 404));
+        return next(new AppError('No tour found with this ID', 404));
     }
 
     res.status(200).json({
