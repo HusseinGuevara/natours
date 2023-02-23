59bff0a8fb4f7c03dece2c0ea8f4c46bdac9c928
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2023-02-22 19:13:31 -0800

    Add files via upload

--- reviewRoutes.js
+++ reviewRoutes.js
@@ -6,19 +6,21 @@ const authController = require('./../controllers/authController.js');
 // mergerParams allows us to merger the parameters from the tourRouter that we mounted reviewRouter to
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
     .route('/')
     .get(reviewController.getAllReviews)
     .post(
-        authController.protect, 
-        authController.restrictTo('user'),
+         authController.restrictTo('user'),
         reviewController.setTourUserIds,
         reviewController.createReview
     )
 
 router
     .route('/:id')
-    .patch(reviewController.updateReview)
-    .delete(reviewController.deleteReview)  
+    .get(reviewController.getReview)
+    .patch(authController.restrictTo('user', 'admin'), reviewController.updateReview)
+    .delete(authController.restrictTo('user', 'admin'), reviewController.deleteReview)  
 
 module.exports = router;    
